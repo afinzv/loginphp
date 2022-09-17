fb910c609b8d8eb6eec94ae8feb32d9e7400e3e8
afinzv <70389927+afinzv@users.noreply.github.com>
2022-09-17 11:11:34 +0530

    Update validation.php

--- validation.php
+++ validation.php
@@ -4,17 +4,20 @@ $con=mysqli_connect('localhost','root','Zedvee@77');
 mysqli_select_db($con,'phpproject');
 $name=$_POST['user'];
 $password=$_POST['password'];
-$S="select * from users where username='$name' and password='$password'";
+$S="select * from users where username='$name'";
 $result=mysqli_query($con,$S);
 $num=mysqli_num_rows($result);
-
-if ($num==1){
-    while ($row = $result->fetch_assoc()) {
-       $_SESSION['user']= $row['full_name'];
+$row = $result->fetch_assoc();
+if($num==1){
+    if ($password==$row['password']){
+        $_SESSION['user']= $row['full_name'];
+        header("Location:home.php");
     }
-    header("Location:home.php");
+    else{
+        echo 'Invalid Password';
+    }
 }
 else{
-    echo "Invalid Credentials";
+    echo "Invalid Username";
 }
 ?>
